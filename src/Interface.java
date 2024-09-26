a6ec22aa9af0e7e1f5c9662ecd23b2b156f6bb96
Prateek Singh <prateeksingh3102000@gmail.com>
2024-09-26 23:20:45 +0530

    Update Interface

--- src/Interface.java
+++ src/Interface.java
@@ -1,5 +1,51 @@
+interface Bicycle{
+    // a = 10 is a by default final
+    int a = 10;
+    // method
+    void applyBreak(int decrement);
+    void speedUp(int increment);
+}
+
+interface HornBicycle{
+void peepee();
+void poopoo();
+}
+
+class AtlasCycle implements Bicycle, HornBicycle{
+    void blowHorn(){
+        System.out.println("Poo Poo Poo Poo");
+    }
+    @Override
+    // Jab hum interface ke method ko implment karte hai toh public banana padta hai
+    public void applyBreak(int decrement){
+        System.out.println("Apply break.");
+    }
+    @Override
+    // Jab hum interface ke method ko implment karte hai toh public banana padta hai
+    public void speedUp(int increment){
+        System.out.println("Apply accelerate.");
+    }
+    @Override
+    public void peepee(){
+        System.out.println("pee pee pee pee");
+    }
+    @Override
+    public void poopoo(){
+        System.out.println("Poo poo poo poo");
+    }
+}
 public class Interface {
     public static void main(String[] args) {
+    AtlasCycle myCycle = new AtlasCycle();
+    myCycle.applyBreak(1);
+    // You can create properties in interface
+    System.out.println(myCycle.a);
 
+    /* You cannot modify the properties in interface as they are final
+    myCycle.a = 20;
+    System.out.println(myCycle.a);
+     */
+     myCycle.poopoo();
+     myCycle.peepee();
     }
 }
