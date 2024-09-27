b261a3b7e3e4c7390636a99fbc07387f97b02220
Prateek Singh <prateeksingh3102000@gmail.com>
2024-09-28 00:34:42 +0530

    Update DefaultMethod.java

--- src/DefaultMethod.java
+++ src/DefaultMethod.java
@@ -1,2 +1,42 @@
+interface MyCamera{
+    void takeSnap();
+    void recordVideo();
+}
+
+interface Mywifi{
+    String[] getNetwork();
+    void connectToNetwork(String network);
+}
+
+// class
+class MyCellPhone{
+    // method
+    void callNumber(int phoneNumber){
+        System.out.println("Calling "+phoneNumber);
+    }
+    // method
+    void PickCall(){
+        System.out.println("Connecting...");
+    }
+}
+class MySmartPhone extends MyCellPhone implements MyCamera, Mywifi{
+    public void TakeSnap(){
+        System.out.println("Taking snap");
+    }
+    public void recordVideo(){
+        System.out.println("Recording video");
+    }
+    public String[] getNetwork(){
+        System.out.println("Getting list of network");
+        String[] networkList = {"Alex", "Mike", "Peter", "Henrry"};
+        return networkList;
+    }
+    public void connectToNetwork(String network){
+        System.out.println("Connecting to "+network);
+    }
+}
 public class DefaultMethod {
+    public static void main(String[] args) {
+
+    }
 }
