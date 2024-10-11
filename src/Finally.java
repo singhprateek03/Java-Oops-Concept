328ff590949acda4c935974544bd49f1e433c8e1
Prateek Singh <prateeksingh3102000@gmail.com>
2024-10-11 23:36:35 +0530

    Create Finally.java

---
+++ src/Finally.java
@@ -0,0 +1,37 @@
+public class Finally {
+    public static int greet(){
+        try{
+            int a = 10;
+            int b = 2;
+            int c = a/b;
+            return c;
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        // Exception aaye chache naa aaye finally method chalega hee chalega
+        finally {
+            System.out.println("End of the greet method.");
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+       int c = greet();
+        System.out.println(c);
+
+        int a = 10;
+        int b = 5;
+        while (true) {
+            try {
+                System.out.println(a / b);
+            }
+            catch (Exception e){
+                System.out.println(e);
+                break;
+            }
+            finally {
+                System.out.println("value of b = "+b);
+            }
+        }
+    }
+}
