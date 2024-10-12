268c72e97232b6348fe0dcb60e2cddbc6cc9d9df
Prateek Singh <prateeksingh3102000@gmail.com>
2024-10-13 01:09:37 +0530

    Update Collection_LinkedList.java

--- src/Collection_LinkedList.java
+++ src/Collection_LinkedList.java
@@ -9,10 +9,12 @@ public class Collection_LinkedList {
         l1.add(8);
         l1.add(54);
         l1.add(65);
+        l1.addFirst(1);     // firstLast() method LinkedList ke starting main given element ko add kar dega
+        l1.addLast(100);    // addLast() method LinkedList ke last main given element ko add kar dega
         l1.add(0,89); // Agar same index pe element add karenge toh wo next index pe shift ho jayega
         l1.add(0,90);
 
-        l2.add(1);
+        l2.add(2);
         l2.add(5);
         l2.add(8);
         l2.add(6);
@@ -25,13 +27,13 @@ public class Collection_LinkedList {
 //        l1.addAll(l2);  // addAll() method l1 and l2 ke saare elements ko add kar dega
 //        l1.clear();   // Clear() method saare elements ko clear kar dega
 //        l1.set(2,80);   // set() method given index pe value ko set kar deta hai
-//        System.out.println(l1.contains(100));    // contains() method check karta hai ki given element ArrayList main hai ki nhi hai
+//        System.out.println(l1.contains(100));    // contains() method check karta hai ki given element LinkedList main hai ki nhi hai
 //        System.out.println("Element present at index: "+l1.indexOf(8));     // indexOf() method batata hai element konse index par hai
 //        System.out.println("Element Last at index: "+l1.lastIndexOf(8));   //  lastIndexOf() method batata hai ki element last time kis index pe aaya tha
 //        System.out.println(l1.remove(5));   // remove() method given index se value ko remove kar deta hai
 
         for(int i=0; i< l1.size();i++){
-            // get() method ArrayList ke element to print karwane ke liye use ho rha hai
+            // get() method LinkedList ke element to print karwane ke liye use ho rha hai
             System.out.print(l1.get(i));
             System.out.print(", ");
         }
