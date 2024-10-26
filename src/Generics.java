4a6bd78eb9587a4bb058cd1b6408c463442e01b6
Prateek Singh <prateeksingh3102000@gmail.com>
2024-10-26 23:58:23 +0530

    Update Generics.java

--- src/Generics.java
+++ src/Generics.java
@@ -2,6 +2,11 @@ import java.util.ArrayList;
 
 public class Generics {
     public static void main(String[] args) {
-        ArrayList arrayList = new ArrayList();
+        ArrayList<Integer> arrayList = new ArrayList();
+//        arrayList.add("Str1");
+        arrayList.add(10);
+        arrayList.add(456);
+        int a = arrayList.get(1);
+        System.out.println(a);
     }
 }
